77deef41e89ad4b38954e86446727c8ff13622cc
h-genie <ses2000hj@hanmail.net>
2022-04-06 22:34:45 +0900

    강의 수강 후 코드

--- 2-3-server-mutation/index.js
+++ 2-3-server-mutation/index.js
@@ -27,26 +27,71 @@ const typeDefs = gql`
         id: String
         team: Int
     }
+    type Mutation {
+      deleteEquipment(id: String): Equipment
+      
+      insertEquipment(
+        id: String,
+        used_by: String,
+        count: Int,
+        new_or_used: String
+      ): Equipment
+      
+      editEquipment(
+        id: String,
+        used_by: String,
+        count: Int,
+        new_or_used: String
+      ): Equipment
+    }
 `
 const resolvers = {
   Query: {
     teams: () => database.teams
-    .map((team) => {
+      .map((team) => {
         team.supplies = database.supplies
-        .filter((supply) => {
+          .filter((supply) => {
             return supply.team === team.id
-        })
+          })
         return team
-    }),
+      }),
     team: (parent, args, context, info) => database.teams
       .filter((team) => {
         return team.id === args.id
       })[0],
     equipments: () => database.equipments,
     supplies: () => database.supplies
+  },
+
+  Mutation: {
+    deleteEquipment: (parent, args, context, info) => {
+      const deleted = database.equipments
+        .filter((equipment) => {
+          return equipment.id === args.id
+        })[0]
+      database.equipments = database.equipments
+        .filter((equipment) => {
+          return equipment.id !== args.id
+        })
+      return deleted
+    },
+
+    insertEquipment: (parent, args, context, info) => {
+      database.equipments.push(args)
+      return args
+    },
+
+    editEquipment: (parent, args, context, info) => {
+      return database.equipments.filter((equipment) => {
+        return equipment.id === args.id
+      }).map((equipment) => {
+        Object.assign(equipment, args)
+        return equipment
+      })[0]
+    },
   }
 }
 const server = new ApolloServer({ typeDefs, resolvers })
 server.listen().then(({ url }) => {
-console.log(`🚀  Server ready at ${url}`)
+  console.log(`🚀  Server ready at ${url}`)
 })
--- 3-2-types/index.js
+++ 3-2-types/index.js
@@ -4,10 +4,12 @@ const queries = require('./typedefs-resolvers/_queries')
 const mutations = require('./typedefs-resolvers/_mutations')
 const equipments = require('./typedefs-resolvers/equipments')
 const supplies = require('./typedefs-resolvers/supplies')
+const enums = require('./typedefs-resolvers/_enums')
 
 const typeDefs = [
     queries,
     mutations,
+    enums,
     equipments.typeDefs,
     supplies.typeDefs
 ]
@@ -17,8 +19,8 @@ const resolvers = [
     supplies.resolvers
 ]
 
-const server =  new ApolloServer({typeDefs, resolvers})
+const server = new ApolloServer({ typeDefs, resolvers })
 
-server.listen().then(({url}) => {
+server.listen().then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`)
 })
